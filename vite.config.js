2d067e069c66bf1a8f9caf4b571dfc021dd75936
roble <renanroble@gmail.com>
2025-07-11 18:08:12 +0100

    feat: configure unplugin-icons with TypeScript support

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
     plugins: [
@@ -15,6 +16,10 @@ export default defineConfig({
                     includeAbsolute: false,
                 },
             },
+        }),
+        Icons({
+            compiler: 'vue3',
+            autoInstall: true,
         }),
     ],
 });
