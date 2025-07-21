cf930deecd049333c14fbd31ea42dbd7f1936f7f
roble <renanroble@gmail.com>
2025-07-21 17:03:55 +0100

    feat: add multi-provider linking and avatar management logic

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::delete('/profile/social/{provider}', [SocialAuthController::class, 'disconnect'])->name('social.disconnect');
 });
 
 // Social login routes
