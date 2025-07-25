569bd131507a565be81073c55bcf2aa76537dfd9
roble <renanroble@gmail.com>
2025-07-25 11:36:16 +0100

    fix: update default app name to 'Sauce Base'

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -6,7 +6,7 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, DefineComponent, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = import.meta.env.VITE_APP_NAME || 'Sauce Base';
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
