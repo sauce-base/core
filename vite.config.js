6dec2ef332a187678fcb1138ebaf22166420e587
roble <renanroble@gmail.com>
2025-08-06 20:17:42 +0100

    feat: update build config for modular architecture

--- vite.config.js
+++ vite.config.js
@@ -3,29 +3,38 @@ import laravel from 'laravel-vite-plugin';
 import path from 'path';
 import Icons from 'unplugin-icons/vite';
 import { defineConfig } from 'vite';
+import collectModuleAssetsPaths from './vite-module-loader.js';
 
-export default defineConfig({
-    plugins: [
-        laravel({
-            input: 'resources/js/app.ts',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
+async function createConfig() {
+    const paths = ['resources/js/app.ts'];
+    const allPaths = await collectModuleAssetsPaths(paths, 'modules');
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: allPaths,
+                refresh: true,
+            }),
+            vue({
+                template: {
+                    transformAssetUrls: {
+                        base: null,
+                        includeAbsolute: false,
+                    },
                 },
+            }),
+            Icons({
+                compiler: 'vue3',
+                autoInstall: true,
+            }),
+        ],
+        resolve: {
+            alias: {
+                '@': path.resolve(__dirname, 'resources/js'),
+                '@modules': path.resolve(__dirname, 'modules'),
             },
-        }),
-        Icons({
-            compiler: 'vue3',
-            autoInstall: true,
-        }),
-    ],
-    resolve: {
-        alias: {
-            '@': path.resolve(__dirname, 'resources/js'),
         },
-    },
-});
+    });
+}
+
+export default createConfig();
