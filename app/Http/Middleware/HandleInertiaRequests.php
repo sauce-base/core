e3c2320131cf8bfa8db65d26d84e9aedc6b66768
roble <renanroble@gmail.com>
2025-09-05 22:09:23 +0100

    refactor: remove appName from shared props in HandleInertiaRequests middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -15,7 +15,6 @@ class HandleInertiaRequests extends Middleware
     public function share(Request $request): array
     {
         return array_merge(parent::share($request), [
-            'appName' => config('app.name'),
             'auth.user' => fn() => $request->user(),
         ]);
     }
