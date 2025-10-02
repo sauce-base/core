b45a2c06e76d5b4b4b558524b3108b4d8a3a3656
roble <renanroble@gmail.com>
2025-10-02 16:14:47 +0100

    feat: add two factor to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,10 +9,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 use Spatie\Permission\Traits\HasRoles;
-use Laravel\Fortify\TwoFactorAuthenticatable;
 
 class User extends Authenticatable implements HasMedia
 {
