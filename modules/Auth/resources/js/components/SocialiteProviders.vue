65bf65ca1ad9a395655077cfeb996ddf82cd8ef0
roble <renanroble@gmail.com>
2025-10-07 16:30:16 +0100

    fix: update background class for improved styling in SocialiteProviders component

--- modules/Auth/resources/js/components/SocialiteProviders.vue
+++ modules/Auth/resources/js/components/SocialiteProviders.vue
@@ -34,9 +34,7 @@ const providers: Provider[] = [
         <div
             class="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"
         >
-            <span
-                class="bg-background text-muted-foreground relative z-10 px-2"
-            >
+            <span class="bg-card text-muted-foreground relative z-10 px-2">
                 Or continue with
             </span>
         </div>
