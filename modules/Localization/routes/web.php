9de95a4d1ffded83e00a8e3565a8d7df728a25a0
roble <renanroble@gmail.com>
2025-10-30 14:49:46 +0000

    chore: remove Localization module (moving to submodule)

--- modules/Localization/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Localization\Http\Controllers\LocalizationController;
-
-Route::any('/locale/{locale}', LocalizationController::class)->name('locale');
