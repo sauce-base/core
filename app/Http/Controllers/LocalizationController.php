7e06f76e1773015907b6995ab3f24e417022ade0
roble <renanroble@gmail.com>
2025-11-03 09:40:47 +0000

    chore: remove npm lint command from setup-env script

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -10,7 +10,7 @@ class LocalizationController
     public function __invoke($locale)
     {
         $availableLocales = array_keys(config('app.available_locales', []));
-        if (!in_array($locale, $availableLocales)) {
+        if (! in_array($locale, $availableLocales)) {
             return response(['error' => 'Invalid locale'], 400);
         }
         App::setLocale($locale);
