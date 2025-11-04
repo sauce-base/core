422b5f6bcc2a99f390e246a74b93e86117577d59
roble <renanroble@gmail.com>
2025-11-04 15:34:08 +0000

    style: adjust spacing in LocalizationController for consistency

--- app/Http/Controllers/LocalizationController.php
+++ app/Http/Controllers/LocalizationController.php
@@ -10,9 +10,11 @@ class LocalizationController
     public function __invoke($locale)
     {
         $availableLocales = array_keys(config('app.available_locales', []));
-        if (!in_array($locale, $availableLocales)) {
+
+        if (! in_array($locale, $availableLocales)) {
             return response(['error' => 'Invalid locale'], 400);
         }
+
         App::setLocale($locale);
         Session::put('locale', $locale);
 
