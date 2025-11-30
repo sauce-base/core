44a672a06ac7c3f3c5ce2d4beba82ad15bddf329
roble <renanroble@gmail.com>
2025-11-30 19:38:10 +0000

    refactor: comment out unused HasRoles trait in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,15 +6,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Modules\Roles\Traits\HasRoles;
+// use Modules\Roles\Traits\HasRoles;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class User extends Authenticatable implements HasMedia
-    // , MustVerifyEmail
+// , MustVerifyEmail
 {
     use HasFactory,
-        HasRoles,
+        // HasRoles,
         InteractsWithMedia,
         Notifiable;
 
