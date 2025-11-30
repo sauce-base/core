54eaa225ea50c16d3ccb573db20424dd9d1fda99
roble <renanroble@gmail.com>
2025-11-30 11:51:58 +0000

    refactor: remove unused middleware setup code

--- resources/js/middleware/index.ts
+++
@@ -1,9 +0,0 @@
-// TODO: add doc to setup middleware and localization
-// import { setupAuthMiddleware } from '@modules/Auth/resources/js/middleware/auth';
-
-/**
- * Initialize all application middleware
- */
-export function setupMiddleware() {
-    // setupAuthMiddleware();
-}
