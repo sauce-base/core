8a83e6d69a5923a630f0e87d7deb9f28a29c1b8a
roble <renanroble@gmail.com>
2025-11-30 18:12:37 +0000

    refactor: improve code formatting and indentation in app.php

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,10 +12,10 @@ use Symfony\Component\HttpFoundation\Response;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         apiPrefix: 'api/v1',
-        commands: __DIR__.'/../routes/console.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
@@ -24,6 +24,13 @@ return Application::configure(basePath: dirname(__DIR__))
             AddLinkHeadersForPreloadedAssets::class,
             HandleLocalization::class,
         ]);
+
+        // Register Spatie Permission Middleware
+        // $middleware->alias([
+        //     'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
+        //     'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
+        //     'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
+        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->respond(function (Response $response, Throwable $exception, Request $request) {
