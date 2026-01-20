76c89146c329a1c17543b12a8c6986a9532f6651
roble <renanroble@gmail.com>
2026-01-20 21:07:50 +0000

    fix: reorder use statements for consistency in routing files

--- modules/NewNavigation/routes/api.php
+++ modules/NewNavigation/routes/api.php
@@ -1,7 +1,7 @@
 <?php
 
-use Modules\NewNavigation\Http\Controllers\NewNavigationController;
 use Illuminate\Support\Facades\Route;
+use Modules\NewNavigation\Http\Controllers\NewNavigationController;
 
 Route::middleware(['auth:sanctum'])->prefix('api/v1/new-navigation')->group(function () {
     Route::apiResource('new-navigation', NewNavigationController::class, ['as' => 'api.new-navigation']);
--- modules/NewNavigation/routes/web.php
+++ modules/NewNavigation/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Modules\NewNavigation\Http\Controllers\NewNavigationController;
 use Illuminate\Support\Facades\Route;
+use Modules\NewNavigation\Http\Controllers\NewNavigationController;
 
 Route::resource('new-navigation', NewNavigationController::class);
