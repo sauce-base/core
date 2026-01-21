0cd514cbeb571075bda1ae76378d98f59b1ec954
roble <renanroble@gmail.com>
2026-01-21 10:05:57 +0000

    chore: remove NewNavigation module and its associated files

--- modules/NewNavigation/routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\NewNavigation\Http\Controllers\NewNavigationController;
-
-Route::middleware(['auth:sanctum'])->prefix('api/v1/new-navigation')->group(function () {
-    Route::apiResource('new-navigation', NewNavigationController::class, ['as' => 'api.new-navigation']);
-});
--- modules/NewNavigation/routes/web.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\NewNavigation\Http\Controllers\NewNavigationController;
-
-Route::resource('new-navigation', NewNavigationController::class);
