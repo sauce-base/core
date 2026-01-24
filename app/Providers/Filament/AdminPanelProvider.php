0f1d97cd621d0c753a1d6df53d19177a4ad0256c
roble <renanroble@gmail.com>
2026-01-24 09:27:25 +0000

    feat: enable SPA mode in AdminPanelProvider

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
         return $panel
             ->id('admin')
             ->path('admin')
+            ->spa()
             ->sidebarCollapsibleOnDesktop()
             ->userMenuItems([
                 Action::make('site')
